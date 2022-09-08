8bec188f3888d290717443dffdd9fe8992666ce7
unknown <al4nlaz0@gmail.com>
2022-09-08 12:51:00 -0300

    add ejemplos de directivas

--- src/app/modules/lists/lista-basica/lista-basica.component.ts
+++ src/app/modules/lists/lista-basica/lista-basica.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+export type Producto = {
+  nombre: string,
+  precio: number,
+  descripcion: string
+}
+
+
 @Component({
   selector: 'app-lista-basica',
   templateUrl: './lista-basica.component.html',
@@ -7,9 +14,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaBasicaComponent implements OnInit {
 
+  listaDeElementos : Producto[]=[
+    {
+      nombre: 'Leche',
+      precio: 10,
+      descripcion: 'Leche Entera'
+    },
+    {
+      nombre: 'Carne',
+      precio:  20,
+      descripcion: 'Carne de vaca'
+    },
+    {
+      nombre: 'Coca Cola',
+      precio: 15,
+      descripcion: 'Coca cola con azucar'
+    }
+  ];
+  cargando :boolean =false;
+  opcion:number | undefined;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  cambiarCargando():void{
+    this.cargando = !this.cargando;
+  }
+
+  escogerOpcion(opcionEscogida:number){
+    this.opcion=opcionEscogida;
+    console.log(this.opcion);
   }
 
 }
