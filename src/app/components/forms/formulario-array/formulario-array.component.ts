22bc74aa56ae3d84b3b7c1515df8553e00bec663
Alan Lazo <al4nlaz0@gmail.com>
2022-09-12 12:41:37 -0300

    add array form

--- src/app/components/forms/formulario-array/formulario-array.component.ts
+++ src/app/components/forms/formulario-array/formulario-array.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder,FormGroup, FormArray } from '@angular/forms';
+
 @Component({
   selector: 'app-formulario-array',
   templateUrl: './formulario-array.component.html',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioArrayComponent implements OnInit {
 
-  constructor() { }
+  miFormularioArray: FormGroup = new FormGroup({})
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormularioArray = this.formBuilder.group({
+      nombre: '',
+      apellido: '',
+      telefonos: this.formBuilder.array([])//Inicializamos la lista de telefonos vacia
+    })
   }
 
+  //Metodo Getter para obtener el FormArray de la lista de telefonos
+  get telefonosFormulario(): FormArray{
+    return  this.miFormularioArray.get('telefonos') as FormArray
+  }
+
+  //Metodo para añadir telefonos a la lista
+  anadirTelefono(){
+    //Creamos un grupo telefono
+    const telefonoNuevo =  this.formBuilder.group({
+      prefijo:'',
+      numero:''
+    })
+
+    this.telefonosFormulario.push(telefonoNuevo);
+  }
+
+  //Metodo para eliminar telefonos de la lista
+  eliminarTelefono(index: number){
+    this.telefonosFormulario.removeAt(index);
+  }
 }
