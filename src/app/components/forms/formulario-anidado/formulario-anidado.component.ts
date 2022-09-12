148cba947f52e9458339c61238a76fd1161cbf0c
Alan Lazo <al4nlaz0@gmail.com>
2022-09-12 12:08:50 -0300

    add se agrego formulario anidado

--- src/app/components/forms/formulario-anidado/formulario-anidado.component.ts
+++ src/app/components/forms/formulario-anidado/formulario-anidado.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+//Importamos de Reactive Form para crear un formulario anidado
+import { FormBuilder,FormGroup } from '@angular/forms';
+
+
 @Component({
   selector: 'app-formulario-anidado',
   templateUrl: './formulario-anidado.component.html',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioAnidadoComponent implements OnInit {
 
-  constructor() { }
+  miFormularioAnidado: FormGroup = new FormGroup({});
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    //Agrupacion para telefonos
+    const telefonoFijo = this.formBuilder.group({
+      prefijo: '',
+      numero:'',
+
+    })
+
+    const telefonoMovil = this.formBuilder.group({
+      prefijo: '',
+      numero:'',
+
+    })
+
+    //Agrupacion de formulario que contine dos agrupaciones
+    this.miFormularioAnidado = this.formBuilder.group({
+      telefonoFijo: telefonoFijo,
+      telefonoMovil: telefonoMovil
+    })
+
+
   }
 
 }
