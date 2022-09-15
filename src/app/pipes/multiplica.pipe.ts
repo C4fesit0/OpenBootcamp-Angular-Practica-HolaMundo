49d16963a6438db4a15ebaa37afe4bf4ceb0be81
Alan Lazo <al4nlaz0@gmail.com>
2022-09-14 23:45:23 -0300

    add multiplica pipe

--- src/app/pipes/multiplica.pipe.ts
+++ src/app/pipes/multiplica.pipe.ts
@@ -5,8 +5,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MultiplicaPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: number, ...args: any): number {
+
+    let x: number = parseFloat(args);
+
+
+    return value * x;
   }
 
 }
